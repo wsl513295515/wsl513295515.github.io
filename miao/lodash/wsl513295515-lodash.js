05afb4e2288225b811b0855e99c994700f1c9b50
wushilu <513295515@qq.com>
2019-07-16 16:58:00 +0800

    filter

--- miao/lodash/wsl513295515-lodash.js
+++ miao/lodash/wsl513295515-lodash.js
@@ -7,5 +7,33 @@ var wsl513295515 = {
       return func(...args.reverse())
     }
   },
-  
+  before: function(n, func){
+    var times = 0
+    return function(...args){
+      times++
+      if(times < n){
+        return lastResult == func(...args)
+      }else{
+        return lastResult
+      }
+    }
+  },
+  unary: function(f){
+    return ary(f,1)
+  },
+  spread: function (f){
+    return function(ary){
+      return f.apply(null,ary)
+    }
+  },
+  filter: function(array,test){
+    var psssed = []
+    for(var i = 0; i < array.length; i++){
+      if(test(array[i])){
+        passed.push(array[i])
+      }
+    }
+    return psssed
+  }
+
 }
