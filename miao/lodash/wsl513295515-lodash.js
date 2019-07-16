fdfeec5c01ff1351aac8421d40510a4261e27586
wushilu <513295515@qq.com>
2019-07-16 15:11:37 +0800

    flip

--- miao/lodash/wsl513295515-lodash.js
+++ miao/lodash/wsl513295515-lodash.js
@@ -2,4 +2,10 @@ var wsl513295515 = {
   compact: function (ary) {
     return ary.filter(it => it)
   },
+  flip: function (func) {
+    return function(...args) {
+      return func(...args.reverse())
+    }
+  },
+  
 }
